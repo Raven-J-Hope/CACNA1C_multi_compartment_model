00a93b75d5735a9825e1d1c8fb61ea562361d1cb
Raven J. Hope <ravenjhope@yahoo.com>
2026-01-27 16:39:12 +0000

    also note for 28/01 - need to make more realistic & figure out how link to real morpho

--- Base_model.py
+++ Base_model.py
@@ -74,6 +74,7 @@ class DGGranuleLikeCell:
         self.axon = h.Section(name=f"{name}.axon")
 
         #connects topology
+        #note to sen, how link to morph? e.g., like trees/T2N/beining?
         self.dend_prox.connect(self.soma(1))
         self.dend_dist.connect(self.dend_prox(1))
         self.axon.connect(self.soma(0))
