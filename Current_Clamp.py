e9a548fb592fdc4d116a245deec68f240a02d56d
Raven J. Hope <ravenjhope@yahoo.com>
2026-02-02 21:54:05 +0000

    spelling

--- Current_Clamp.py
+++ Current_Clamp.py
@@ -344,7 +344,7 @@ if __name__ == "__main__":
     cell2.setup_recording()
     t1, vs1, vp1, vd1, vsp1, cai1_soma, cai1_prox, cai1_dist, cai1_spine = run_sim(cell2, tstop=500, v_init=-70, dt=0.025)
     print("lens:", len(t1), len(vs1), len(vp1), len(vd1), len(vsp1))
-    print("Peak cai 50% soma/prox/dist/spine:",  # prints ca peak in each compartmnet
+    print("Peak cai 50% soma/prox/dist/spine:",  #prints ca peak in each compartment
           float(np.max(cai1_soma)),
           float(np.max(cai1_prox)),
           float(np.max(cai1_dist)),
