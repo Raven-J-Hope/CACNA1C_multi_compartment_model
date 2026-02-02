21194a887b292b5c21abaded35870369a1d4b0f2
Raven J. Hope <ravenjhope@yahoo.com>
2026-02-02 21:57:32 +0000

    added units to remember in #

--- Voltage_Clamp.py
+++ Voltage_Clamp.py
@@ -349,7 +349,7 @@ if __name__ == "__main__":
     cell.add_voltage_clamp(hold=-70.0, step=-50.0, delay=100, dur=300)
     cell.setup_recording()
     t0, vs0, vp0, vd0, vsp0, cai0_soma, cai0_prox, cai0_dist, cai0_spine = run_sim(cell, tstop=500, v_init=-70, dt=0.025)
-    I0 = np.array(cell.iclamp_vec) if cell.iclamp_vec is not None else None  #adds current arrays so can plot
+    I0 = np.array(cell.iclamp_vec) if cell.iclamp_vec is not None else None  #adds current arrays so can plot, in nA
     print("lens:", len(t0), len(vs0), len(vp0), len(vd0), len(vsp0))
     print("Peak cai base soma/prox/dist/spine:", #prints ca peak in each compartmnet
           float(np.max(cai0_soma)),
