33eaefa0ee0e02d75c89f1a345145d2386e2e3af
Raven J. Hope <ravenjhope@yahoo.com>
2026-02-19 16:50:07 +0000

    .

--- Current_Clamp.py
+++ Current_Clamp.py
@@ -406,7 +406,7 @@ class DGGranuleLikeCell:
         self._set_channel_densities_default()
 #set baseline conductances
     def _set_channel_densities_default(self):
-       for sec in [self.soma, self.dend_prox, self.dend_dist]:
+        for sec in [self.soma, self.dend_prox, self.dend_dist]:
             for seg in sec:
                 if sec is self.soma:
                     scale = 1.0
