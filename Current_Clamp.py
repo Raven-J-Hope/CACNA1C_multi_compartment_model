ee605051c36364ab60db876ff369df3f717b0c91
Raven J. Hope <ravenjhope@yahoo.com>
2026-02-19 16:52:01 +0000

    added currents

--- Current_Clamp.py
+++ Current_Clamp.py
@@ -678,7 +678,7 @@ if __name__ == "__main__":
     cell2.scale_cav12(0.5)
     cell2.add_current_clamp(delay=100, dur=300, amp=0.2)
     cell2.setup_recording()
-    t1, vs1, vp1, vd1, vsp1, cai1_soma, cai1_prox, cai1_dist, cai1_spine = run_sim(cell2, tstop=500, v_init=-70, dt=0.025)
+    t1, vs1, vp1, vd1, vsp1, cai1_soma, cai1_prox, cai1_dist, cai1_spine, ica1_soma, ik1_soma, bkik1_soma = run_sim(cell2, tstop=500, v_init=-70, dt=0.025)
     print("lens:", len(t1), len(vs1), len(vp1), len(vd1), len(vsp1))
     print("Peak cai 50% soma/prox/dist/spine:",  #prints ca peak in each compartment
           float(np.max(cai1_soma)),
