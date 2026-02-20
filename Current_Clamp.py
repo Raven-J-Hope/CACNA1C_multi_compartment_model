3c2f8809cf7bf65d53f9d9d54c09a0f2e60c6024
Raven J. Hope <ravenjhope@yahoo.com>
2026-02-20 16:25:02 +0000

    removed trail ;

--- Current_Clamp.py
+++ Current_Clamp.py
@@ -231,7 +231,7 @@ class DGGranuleLikeCell:
             try_insert(head, "Caold")
             try_insert(neck, "Cabuffer")
             try_insert(head, "Cabuffer")
-            try_insert(neck, "Cav12");
+            try_insert(neck, "Cav12")
             try_insert(head, "Cav12")
             try_insert(neck, "BK");
             try_insert(head, "BK")
