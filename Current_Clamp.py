3e1f2dd8dfcde70ac472fe62d92874130b6c8b40
Raven J. Hope <ravenjhope@yahoo.com>
2026-03-25 12:51:18 +0000

    cav21

--- Current_Clamp.py
+++ Current_Clamp.py
@@ -766,6 +766,11 @@ class DGGranuleLikeCell:
                     seg.Cabuffer.tau = 9.0  #ms
                     seg.Cabuffer.brat = 1.0  #buffer ratio factor
 
+                #Cav2.1 permeability density, cm/s
+                if has_mech(sec, "Cav2_1"):
+                    seg.Cav2_1.pcabar = 2.2e-6 * scale  #-6 = like w/ no cav21, -5 big diff & -4 break
+                    seg.Cav2_1.vshift = 0.0
+
     def scale_cav12(self, factor: float):
         for sec in [self.soma, self.dend_prox, self.dend_dist] +self.spine_necks +self.spines:
             if has_mech(sec, "Cav12"):
