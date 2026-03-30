99bfb49628c616a908baccc2364a5334bbfcb739
Raven J. Hope <ravenjhope@yahoo.com>
2026-03-30 11:16:52 +0100

    bk split

--- Current_Clamp.py
+++ Current_Clamp.py
@@ -742,10 +742,11 @@ class DGGranuleLikeCell:
                     seg.BK_Cav22.gakbar = total_bk_gakbar * self.bk_split["BK_Cav22"]
                     seg.BK_Cav22.gabkbar = total_bk_gabkbar * self.bk_split["BK_Cav22"]
 
-                #BK - Cav21
-                if has_mech(sec, "BK_Cav21"):
-                    seg.BK_Cav21.gakbar = 1e-4 * scale
-                    seg.BK_Cav21.gabkbar = 1e-4 * scale
+                    seg.BK_Cav12.gakbar = total_bk_gakbar * self.bk_split["BK_Cav12"]
+                    seg.BK_Cav12.gabkbar = total_bk_gabkbar * self.bk_split["BK_Cav12"]
+
+                    seg.BK_Cav21.gakbar = total_bk_gakbar * self.bk_split["BK_Cav21"]
+                    seg.BK_Cav21.gabkbar = total_bk_gabkbar * self.bk_split["BK_Cav21"]
 
                 #SK2
                 if has_mech(sec, "SK2"):
