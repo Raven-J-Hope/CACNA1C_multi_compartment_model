3ee4f9cdbc101b001406003fa1603a737417f592
Raven J. Hope <ravenjhope@yahoo.com>
2026-03-30 11:34:36 +0100

    pas.g

--- Voltage_Clamp.py
+++ Voltage_Clamp.py
@@ -193,7 +193,7 @@ class DGGranuleLikeCell:
                     seg.pas.e = -70.0
             if has_mech(head, "pas"):
                 for seg in head:
-                    seg.pas.g = 1e-4
+                    seg.pas.g = 0.00039
                     seg.pas.e = -70.0
 
             #give spines Ca mechs
