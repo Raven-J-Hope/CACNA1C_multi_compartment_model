6b8458ac3db2ffb48559c86ae1d39fb8e3bedb2f
Raven J. Hope <ravenjhope@yahoo.com>
2026-03-30 11:39:30 +0100

    typo

--- Voltage_Clamp.py
+++ Voltage_Clamp.py
@@ -456,7 +456,7 @@ class DGGranuleLikeCell:
                     seg.Cav2_1.vshift = 0.0
 
     def scale_cav12(self, factor: float):
-        for sec in [self.soma, self.dend_prox, self.dend_dist] +self.spine_necks +self.spines:
+        for sec in [self.soma, self.dend_prox, self.dend_dist] + self.spine_necks + self.spines:
             if has_mech(sec, "Cav12"):
                 for seg in sec:
                     seg.Cav12.gbar *= factor
