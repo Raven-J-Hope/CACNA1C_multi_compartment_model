070e7bb2df0b6ae0c248449bd0766668060599ee
Raven J. Hope <ravenjhope@yahoo.com>
2026-03-30 11:27:33 +0100

    label update

--- Current_Clamp.py
+++ Current_Clamp.py
@@ -2131,7 +2131,7 @@ if __name__ == "__main__":
         plt.plot(t1, cai1_dist, color=CAV12_50_COLOR, label=CAV12_50_LABEL)
         plt.xlabel("Time (ms)")
         plt.ylabel("cai (mM)")
-        plt.title("Baseline vs reduced Cav1.2 intracellular Ca at dist dend")
+        plt.title("Intracellular Ca at dist dend")
         plt.legend()
         plt.tight_layout()
         plt.savefig(os.path.join(FIG_DIR, "#plot cai comparison in distal dendrite.png"), dpi=300)
