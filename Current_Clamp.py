ff49fcff56fdcb8575d5bf7fd5e8923bffd2699a
Raven J. Hope <ravenjhope@yahoo.com>
2026-04-08 17:22:00 +0100

    updated sanity

--- Current_Clamp.py
+++ Current_Clamp.py
@@ -1591,7 +1591,7 @@ if __name__ == "__main__":
         print(f"Has {mech} on soma?", h.ismembrane(mech, sec=cell.soma))
 
     #mechanism sanity checks - prox dend sanity
-    for mech in ["BK_Cav22", "BK_Cav12", "BK_Cav21", "Cav12", "Cav22", "Cav32", "SK2", "HCN", "Cabuffer", "Caold", "Kv42", "Kv11", "ichan3", "Kir21",
+    for mech in ["BK_Cav22", "BK_Cav12", "BK_Cav21", "Cav12", "Cav13", "Cav22", "Cav32", "SK2", "HCN", "Cabuffer", "Caold", "Kv42", "Kv11", "ichan3", "Kir21",
                  "Kv14", "Kv21", "Kv33", "Kv34", "Kv42b", "Kv723", "na8st", "Cav2_1"]:
         print(f"Has {mech} on prox dend?", h.ismembrane(mech, sec=cell.dend_prox))
 
