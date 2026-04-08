b5a9e19f820cc96c34904278437d94bc7911fe8d
Raven J. Hope <ravenjhope@yahoo.com>
2026-04-08 17:34:39 +0100

    #

--- Voltage_Clamp.py
+++ Voltage_Clamp.py
@@ -1169,7 +1169,7 @@ plt.show()
 #4-panel Cav1.3 source current by compartment
 fig, axes = plt.subplots(2, 2, figsize=(12, 9), sharex=True, sharey=False)
 
-# AIS
+#AIS
 ax = axes[0, 0]
 ax.plot(t0, cav13_ica0_ais, color=WT_COLOR, label=WT_LABEL)
 ax.plot(t1, cav13_ica1_ais, color=CAV12_50_COLOR, label=CAV12_50_LABEL)
