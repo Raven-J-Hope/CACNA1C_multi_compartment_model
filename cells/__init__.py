8e48a2f8bb4010bff2744817e25c554ecc87b5f3
Raven J. Hope <ravenjhope@yahoo.com>
2026-04-09 21:14:16 +0100

    add alias too

--- cells/__init__.py
+++ cells/__init__.py
@@ -1,4 +1,4 @@
-from .wt_cell import DGGranuleLikeCell, WT_BK_SPLIT, CAV12_50_BK_SPLIT
+from .wt_cell import WTCell, DGGranuleLikeCell, WT_BK_SPLIT, CAV12_50_BK_SPLIT
 from .cav12_50_cell import Cav12_50Cell
 
 #aliases
